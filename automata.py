37010d3d2ee66e12497e97233a3e5ea5da0179d0
fcgriff-html <80140506+fcgriff-html@users.noreply.github.com>
2021-05-07 20:03:54 -0400

    Update automata.py

--- automata.py
+++ automata.py
@@ -1,14 +1,81 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr  3 15:19:26 2021
+Created on Fri May  7 19:15:10 2021
 
+@author: charl
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  3 15:19:26 2021
 @author: charl
 """
 
 """
 ok so basically all we need to do is repurpose this state machine to start scanning once the proper state has been reached, we can have it start a quiz or something, it shouldnt be too crazy
 """
+def calc_math(dicty, key_idx, mathstr):
+    total = int(mathstr[0])
+    for i in range(len(mathstr)):
+        if dicty[key_idx[i]]:
+            res = dicty[key_idx[i]]
+            if res == "plus":
+               int2 = int(mathstr[i+2])
+               total = total + int2
+            elif res == "minus":
+                int2 = int(mathstr[i+1])
+                total = total - int2
+            elif res == "divide":
+                int2 = int(mathstr[i+1])
+                total = total / int2
+            elif res == "multiply":
+                int2 = int(mathstr[i+1])
+                total = total * int2
+    print(total)
+            
+
+def scanner():
+    print("format note: please use '-' to denote math, and '--' to denote a dash", "\n")
+    print("if you decide to use numbers in your string, we can also compute those values")
+    scan = input("please enter the string to be scanned: ")
+    calc = input("would you like to see the result(numbers only, no spaces or letters)? y/n: ")
+    output = switcher(scan) #pseudo switch case to determine the token
+    #output is a dictionary organized by index in the string
+    token_idx = []
+    keys = output.keys()
+    for key in keys:
+        token_idx.append(key)
+    for i in range(len(token_idx)):
+        if i == 0:
+            print(scan[0:token_idx[i+1]], "  " , output[token_idx[i]])
+        elif i < len(token_idx)-1:
+            print(scan[token_idx[i]:token_idx[i+1]], "  ", output[token_idx[i]])
+        else:
+            print(scan[token_idx[i]::], "  ", output[token_idx[i]])
+    if calc == "y":
+        calc_math(output, token_idx, scan)
+
+            
+def switcher(scan):
+        #takes string in, returns array w/ token types and indexs for each token
+        ret = {}
+        for i in range(len(scan)):
+            if scan[i] == "+":
+                ret[i] = "plus"
+            if scan[i] == "-":
+                ret[i] = "minus"
+            if scan[i] == "/":
+                ret[i] = "divide"
+            if scan[i] == "*":
+                ret[i] = "multiply"
+            if scan[i] == "(":
+                ret[i] = "open parens"
+            if scan[i] == ")":
+                ret[i] = "closed parens"
+                
+        return ret
 def dfa_model():
+    print("even zeros, odd ones :)")
     print("please enter your string: ")
     input_string = input()
     dfa_states= {0:{'0':0, '1':3},
@@ -20,7 +87,7 @@ def dfa_model():
         state = dfa_states[state][i]
     if state == 1:
         print("accepted!   " + input_string)
-        print("/n" + "ready to unlock the scanner? y/n: ")
+        print("\n" + "ready to unlock the scanner? y/n: ")
         answer = input()
         if answer == "y":
             scanner()
@@ -34,42 +101,3 @@ def dfa_model():
             dfa_model()
         else:
             print("thanks for playing!")
-    
-  def scanner():
-    print("format note: please use '-' to denote math, and '--' to denote a dash", "/n")
-    scan = input("please enter the string to be scanned: ")
-    output = switcher(scan) #pseudo switch case to determine the token
-    #output is a dictionary organized by index in the string
-    token_idx = output.keys()
-    for i in range(len(token_idx)):
-        if i == 0:
-            print(scan[0:i], "  " , output[token_idx[i]])
-        elif i < len(token_idx)-1:
-            print(scan[i:i+1], "  ", output[token_idx[i]])
-        else:
-            print(scan[i::], "  ", output[token_idx[i]])
-
-            
- def switcher(scan):
-    #takes string in, returns array w/ token types and indexs for each token
-    ret = {}
-    for i in range(len(scan)):
-        if scan[i] == "+":
-            ret[i] = "plus"
-        if scan[i] == "-":
-            ret[i] = "minus"
-        if scan[i] == "/":
-            ret[i] = "divide"
-        if scan[i] == "*":
-            ret[i] = "multiply"
-        if scan[i] == "--":
-            ret[i] = "dash"
-        if scan[i] == "**":
-            ret[i] = "exponet"
-    return ret
-            
-    
-    
-    
-    
-        
